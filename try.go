632d7f59a8ef3da921677917e2d876b95fb18a36
lufia <lufia@lufia.org>
2025-10-20 22:33:55 +0900

    add Rewinder

--- try.go
+++ try.go
@@ -60,24 +60,28 @@ func (s *Scope) raise(skip int, err error) {
 	panic("do not reach here")
 }
 
+type Rewinder func(*Scope, ...Option)
+
 // Check checks whether err is not nil.
 // If err is nil, it does nothing.
 // Otherwise it rewinds to the fallback point s, then [Handle] returns err.
 //
 // Check should be called on the same stack to [Handle].
-func Check(err error) func(*Scope, ...Option) {
+func Check(err error) Rewinder {
 	return func(s *Scope, opts ...Option) {
 		applyOpts(s, opts...)
 		s.raise(1, err)
 	}
 }
 
+type Rewinder1[T any] func(*Scope, ...Option) T
+
 // Check1 checks whether err is not nil.
 // If err is nil, it returns v.
 // Otherwise it rewinds to the fallback point s, then [Handle] returns err.
 //
 // Check1 should be called on the same stack to [Handle].
-func Check1[T any](v T, err error) func(*Scope, ...Option) T {
+func Check1[T any](v T, err error) Rewinder1[T] {
 	return func(s *Scope, opts ...Option) T {
 		applyOpts(s, opts...)
 		s.raise(1, err)
@@ -85,8 +89,10 @@ func Check1[T any](v T, err error) func(*Scope, ...Option) T {
 	}
 }
 
+type Rewinder2[T1, T2 any] func(*Scope, ...Option) (T1, T2)
+
 // Check2 is a variant of [Check1].
-func Check2[T1, T2 any](v1 T1, v2 T2, err error) func(*Scope, ...Option) (T1, T2) {
+func Check2[T1, T2 any](v1 T1, v2 T2, err error) Rewinder2[T1, T2] {
 	return func(s *Scope, opts ...Option) (T1, T2) {
 		applyOpts(s, opts...)
 		s.raise(1, err)
